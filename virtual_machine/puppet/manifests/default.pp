b429b58d893c08a2fd5e311cbe61dc527c7b708b
Chris Johnson <chris@johnsonch.com>
2015-01-06 10:43:05 -0600

    work on virtual machine and just linked to rails cookbook

--- virtual_machine/puppet/manifests/default.pp
+++ virtual_machine/puppet/manifests/default.pp
@@ -88,10 +88,15 @@ package { 'nodejs':
 
 # --- Ruby ---------------------------------------------------------------------
 
+exec { 'keyserver':
+  command => "${as_vagrant} 'gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3'",
+  require => Package['curl']
+}
+
 exec { 'install_rvm':
   command => "${as_vagrant} 'curl -L https://get.rvm.io | bash -s stable'",
   creates => "${home}/.rvm/bin/rvm",
-  require => Package['curl']
+  require => Exec['keyserver']
 }
 
 exec { 'install_ruby':
